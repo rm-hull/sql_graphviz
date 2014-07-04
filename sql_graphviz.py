6c0d5173ba596b252bfbe29d8f9e5ce9fd96da4f
Richard Hull <rm_hull@yahoo.co.uk>
2014-07-04 18:39:23 +0100

    PEP8 formatting

--- sql_graphviz.py
+++ sql_graphviz.py
@@ -1,35 +1,55 @@
 #!/usr/bin/python
-from pyparsing import Literal, CaselessLiteral, Word, delimitedList ,Optional, Combine, Group, alphas, nums, alphanums, Forward,oneOf, sglQuotedString, OneOrMore, ZeroOrMore, CharsNotIn         ,Suppress
+from pyparsing import Literal, CaselessLiteral, Word, delimitedList, Optional, Combine, Group, alphas, nums, alphanums, Forward, oneOf, sglQuotedString, OneOrMore, ZeroOrMore, CharsNotIn, Suppress
+
 
 def field_act(s, loc, tok):
     return ("<" + tok[0] + "> " + " ".join(tok)).replace("\"", "\\\"")
 
+
 def field_list_act(s, loc, tok):
     return " | ".join(tok)
+
 
 def create_table_act(s, loc, tok):
     return """"%(tableName)s" [\n\t label="<%(tableName)s> %(tableName)s | %(fields)s"\n\t shape="record"\n];""" % tok
 
+
 def add_fkey_act(s, loc, tok):
     return """ "%(tableName)s":%(keyName)s -> "%(fkTable)s":%(fkCol)s """ % tok
+
 
 def other_statement_act(s, loc, tok):
     return ""
 
+
 def grammar():
     parenthesis = Forward()
-    parenthesis <<= "(" + ZeroOrMore(CharsNotIn("()") | parenthesis)  + ")"
+    parenthesis <<= "(" + ZeroOrMore(CharsNotIn("()") | parenthesis) + ")"
 
-    field_def = OneOrMore(Word(alphanums+"_\"':-") | parenthesis)
+    field_def = OneOrMore(Word(alphanums + "_\"':-") | parenthesis)
     field_def.setParseAction(field_act)
 
     field_list_def = field_def + ZeroOrMore(Suppress(",") + field_def)
     field_list_def.setParseAction(field_list_act)
 
-    create_table_def = Literal("CREATE") + "TABLE" + Word(alphas+"_").setResultsName("tableName") + "(" + field_list_def.setResultsName("fields") + ")" + ";"
+    create_table_def = Literal(
+        "CREATE") + "TABLE" + Word(
+        alphas + "_").setResultsName(
+        "tableName") + "(" + field_list_def.setResultsName(
+        "fields") + ")" + ";"
     create_table_def.setParseAction(create_table_act)
 
-    add_fkey_def = Literal("ALTER") + "TABLE" + "ONLY" + Word(alphanums+"_").setResultsName("tableName") + "ADD" + "CONSTRAINT" + Word(alphanums+"_") + "FOREIGN" + "KEY" + "(" + Word(alphanums+"_").setResultsName("keyName") + ")"     +"REFERENCES"+Word(alphanums+"_").setResultsName("fkTable")+"("+Word(alphanums+"_").setResultsName("fkCol")+")"+";"
+    add_fkey_def = Literal(
+        "ALTER") + "TABLE" + "ONLY" + Word(
+        alphanums + "_").setResultsName(
+        "tableName") + "ADD" + "CONSTRAINT" + Word(
+        alphanums + "_") + "FOREIGN" + "KEY" + "(" + Word(
+        alphanums + "_").setResultsName(
+        "keyName") + ")" + "REFERENCES" + Word(
+        alphanums + "_").setResultsName(
+        "fkTable") + "(" + Word(
+        alphanums + "_").setResultsName(
+        "fkCol") + ")" + ";"
     add_fkey_def.setParseAction(add_fkey_act)
 
     other_statement_def = OneOrMore(CharsNotIn(";")) + ";"
@@ -39,6 +59,7 @@ def grammar():
     comment_def.setParseAction(other_statement_act)
 
     return OneOrMore(comment_def | create_table_def | add_fkey_def | other_statement_def)
+
 
 def graphviz(filename):
     print """digraph g { graph [ rankdir = "LR" ]; """
