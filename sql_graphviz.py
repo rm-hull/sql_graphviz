75aa06b1b3b075381fe821d16f7e241a6453f35f
mefyl <mefyl@gruntech.org>
2019-12-10 10:44:41 +0100

    Use intermediate objects instead of concatenating strings directly.

--- sql_graphviz.py
+++ sql_graphviz.py
@@ -5,36 +5,57 @@ import sys
 from datetime import datetime
 from pyparsing import alphas, alphanums, Literal, Word, Forward, OneOrMore, ZeroOrMore, CharsNotIn, Suppress, QuotedString, Optional
 
+class Field:
 
-def field_act(s, loc, tok):
-    fieldName = tok[0].replace('"', '')
-    fieldSpec = html.escape(' '.join(tok[1::]).replace('"', '\\"'))
-    return '<tr><td bgcolor="grey96" align="left" port="{0}"><font face="Times-bold">{0}</font>  <font color="#535353">{1}</font></td></tr>'.format(fieldName, fieldSpec)
+    def __init__(self, loc, tok):
+        self.name = tok[0].replace('"', '')
+        self.spec = html.escape(' '.join(tok[1::]).replace('"', '\\"'))
 
-
-def field_list_act(s, loc, tok):
-    return "\n        ".join(tok)
-
-
-def create_table_act(s, loc, tok):
-    return '''
-  "{tableName}" [
-    shape=none
-    label=<
-      <table border="0" cellspacing="0" cellborder="1">
-        <tr><td bgcolor="lightblue2"><font face="Times-bold" point-size="20">{tableName}</font></td></tr>
-        {fields}
-      </table>
-    >];'''.format(**tok)
+    def __str__(self):
+        return '<tr><td bgcolor="grey96" align="left" port="{0}"><font face="Times-bold">{0}</font>  <font color="#535353">{1}</font></td></tr>'.format(self.name, self.spec)
 
 
-def add_fkey_act(s, loc, tok):
-    return '  "{tableName}":{keyName} -> "{fkTable}":{fkCol}'.format(**tok)
+class Table:
+
+    def __init__(self, loc, tok):
+        self.name = tok['tableName']
+        self.fields = tok['fields']
+
+    def __str__(self):
+        return '''
+        "{name}" [
+        shape=none
+        label=<
+          <table border="0" cellspacing="0" cellborder="1">
+            <tr><td bgcolor="lightblue2"><font face="Times-bold" point-size="20">{name}</font></td></tr>
+            {fields}
+          </table>
+        >];'''.format(name=self.name, fields="\n        ".join(map(str, self.fields)))
 
 
-def other_statement_act(s, loc, tok):
-    return ""
+class FKey:
 
+    def __init__(self, loc, tok):
+        self.table = tok['tableName']
+        self.key = tok['keyName']
+        self.ftable = tok['fkTable']
+        self.fcol = tok['fkCol']
+
+    def __str__(self):
+        return '  "{table}":{key} -> "{ftable}":{fcol}'.format(
+            table=self.table,
+            key=self.key,
+            ftable=self.ftable,
+            fcol=self.fcol)
+
+
+class OtherStatement:
+
+    def __init__(self, s, loc, tok):
+        pass
+
+    def __str__(self):
+        return ''
 
 def join_string_act(s, loc, tok):
     return "".join(tok).replace('\n', '\\n')
@@ -56,27 +77,29 @@ def grammar():
     quoted_default_value.setParseAction(quoted_default_value_act)
 
     field_def = OneOrMore(quoted_default_value | Word(alphanums + "_\"'`:-/[].") | parenthesis)
-    field_def.setParseAction(field_act)
+    field_def.setParseAction(Field)
 
     tablename_def = ( Word(alphas + "`_.") | QuotedString("\"") )
 
     field_list_def = field_def + ZeroOrMore(Suppress(",") + field_def)
-    field_list_def.setParseAction(field_list_act)
 
     create_table_def = Literal("CREATE") + "TABLE" + tablename_def.setResultsName("tableName") + "(" + field_list_def.setResultsName("fields") + ")" + ";"
-    create_table_def.setParseAction(create_table_act)
+    create_table_def.setParseAction(Table)
 
     add_fkey_def = Literal("ALTER") + "TABLE" + "ONLY" + tablename_def.setResultsName("tableName") + "ADD" + "CONSTRAINT" + Word(alphanums + "_") + "FOREIGN" + "KEY" + "(" + Word(alphanums + "_").setResultsName("keyName") + ")" + "REFERENCES" + Word(alphanums + "._").setResultsName("fkTable") + "(" + Word(alphanums + "_").setResultsName("fkCol") + ")" + Optional(Literal("DEFERRABLE")) + Optional(Literal("ON") + "DELETE" + ( Literal("CASCADE") | Literal("RESTRICT") )) + ";"
-    add_fkey_def.setParseAction(add_fkey_act)
+    add_fkey_def.setParseAction(FKey)
 
     other_statement_def = OneOrMore(CharsNotIn(";")) + ";"
-    other_statement_def.setParseAction(other_statement_act)
+    other_statement_def.setParseAction(OtherStatement)
 
     comment_def = "--" + ZeroOrMore(CharsNotIn("\n"))
-    comment_def.setParseAction(other_statement_act)
+    comment_def.setParseAction(OtherStatement)
 
     return OneOrMore(comment_def | create_table_def | add_fkey_def | other_statement_def)
 
+def parse(filename):
+    parsed = grammar().setDebug(False).parseFile(filename)
+    return parsed
 
 def graphviz(filename):
     print("/*")
@@ -85,9 +108,9 @@ def graphviz(filename):
     print(" */")
     print("digraph g { graph [ rankdir = \"LR\" ];")
 
-    for i in grammar().setDebug(False).parseFile(filename):
-        if i != "":
-            print(i)
+    for stmt in parse(filename);
+        if not isinstance(stmt, OtherStatement):
+            print(stmt)
     print("}")
 
 if __name__ == '__main__':
